cc5d04bcd289ce60daddfa28baad25c320724fc7
sheenabigail <79987823+sheenabigail@users.noreply.github.com>
2022-04-26 19:08:13 +0100

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'PHAS0048 Project'
+copyright = '2022, Sheena Abigail'
+author = 'Sheena Abigail'
 
 release = '0.1'
 version = '0.1.0'
