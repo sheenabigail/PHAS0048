5423024b1c4247d892701bfc99af687039487157
sheenabigail <79987823+sheenabigail@users.noreply.github.com>
2022-04-26 18:07:34 +0100

    Update SH_model.R

--- supp_code/SH_model.R
+++ supp_code/SH_model.R
@@ -1,22 +1,19 @@
 # Find the filenames corresponding to a given J,e/f, and +/-.
 # I generated them using the bash command:
 # for i in `ls *.states`; do grep "      1.5 + f " $i >$i.2 ; done
-setwd("D:/STUDY/UCL/PHAS0048/energyvsbox/rfiles_v2_200points_01reor/")  
-files=list.files(path="D:/STUDY/UCL/PHAS0048/energyvsbox/rfiles_v2_200points_01reor/",pattern = "*.states.2")
+setwd("directory/path/containing/.states.2/files")  
+files=list.files(path="directory/path/containing/.states.2/files",pattern = "*.states.2")
 #Read the last file for getting the number of rows.
-# tmp=read.table(files[max(length(files))])
-tmp=read.table(file = "SH_MRCI_02_L6.480_J10_95ma_v2.states.2")
-# print(tmp)
+tmp=read.table(files[max(length(files))])
 max.dim=length(tmp$V2)
-print(max.dim)
 # Create the matrix and initialize the values with NA (NaN in any other
 # language, but R wants to feel different)
-m=matrix(NA,ncol = 200,nrow = max.dim)
+m=matrix(NA,ncol = "number of states files",nrow = max.dim)
 # step is the increase in the box size.
-step=0.02
+step="value of dl used in do-submit-duo-loop.sh"
 # Read the state files and store energies in matrix
-for (i in seq(1,200,by=1)){ # loop over all the files 
-  box=2.500+(i-1)*0.01 # calculate the box size 
+for (i in seq(1,"number of states files",by=1)){ # loop over all the files 
+  box="value of L used in do-submit-duo-loop.sh"+(i-1)*"value of dl used in do-submit-duo-loop.sh" # calculate the box size 
   data=read.table(files[i]) # read the state file
   print(dim(data))
   m[1:length(data$V2),i]=data$V2 # store the info inside the matrix
@@ -25,7 +22,7 @@ for (i in seq(1,200,by=1)){ # loop over all the files
 mt=t(m)
 mt=na.omit(mt)
 # Store r_box 
-r=seq(2.500,6.480,by=0.02)
+r=seq("value of L used in do-submit-duo-loop.sh","max box size in do-submit-duo-loop.sh",by="value of dl used in do-submit-duo-loop.sh")
 # Export the E vs r_box  for each single state.
 for (i in seq(1,max.dim,1)){
   outname=paste(i,".dat",sep="")
